32ca8bdc334781c65340bbb73cce029ede9d0f5f
martin777pro <martinfuentebiz22@gmail.com>
2024-04-26 18:42:33 -0400

    Snickers y Super8 arreglados

--- ola/src/main/java/org/example/Snickers.java
+++ ola/src/main/java/org/example/Snickers.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Snickers extends Dulce {
     public Snickers(int codigo, String nombre, int precio, String sabor) {
-        super(codigo, nombre, precio, sabor);
+        super(codigo, nombre, Precios.SNICKERS.getPrecio(), sabor);
+    }
+
+    @Override
+    public String consumir() {
+        return "Comiendo Snickers";
     }
 }
