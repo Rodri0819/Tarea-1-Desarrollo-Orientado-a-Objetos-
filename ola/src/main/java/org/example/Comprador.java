67ff1b8f17299baa5caa1c481e7a98820f299836
martin777pro <martinfuentebiz22@gmail.com>
2024-04-26 18:58:19 -0400

    Comprador arreglado

--- ola/src/main/java/org/example/Comprador.java
+++ ola/src/main/java/org/example/Comprador.java
@@ -1,10 +1,10 @@
 package org.example;
 
-public class Comprador {
+class Comprador {
     private String sonido;
     private int vuelto;
 
-    public Comprador(Moneda m, int cualProducto, Expendedor exp) {  // Cambiar cualBebida a cualProducto
+    public Comprador(Moneda m, int cualProducto, Expendedor exp) throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {  // Cambiar cualBebida a cualProducto
         Producto producto = exp.comprarProducto(m, cualProducto);  // Llamar a comprarProducto en lugar de comprarBebida
         if (producto != null) {
             sonido = producto.consumir();  // Llamar al método consumir() de Producto
