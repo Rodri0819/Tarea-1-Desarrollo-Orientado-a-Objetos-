8d9c1e16e61c7d677a5e16307a9cb6ba7255832f
TomasGutierrez777 <togutierrez2023@udec.cl>
2024-04-26 12:33:12 -0400

    CocaCola Arreglada

--- ola/src/main/java/org/example/CocaCola.java
+++ ola/src/main/java/org/example/CocaCola.java
@@ -3,12 +3,12 @@ package org.example;
 public class CocaCola extends Bebida {
     // Constructor
     public CocaCola(int codigo, String nombre, int precio, String marca) {
-        super(codigo, nombre, precio, marca);
+        super(codigo, nombre, Precios.COCA_COLA.getPrecio(), marca);
     }
 
     // Método sobreescrito
     @Override
     public String consumir() {
-        return "Bebiendo CocaCola";
+        return "CocaCola";
     }
 }
